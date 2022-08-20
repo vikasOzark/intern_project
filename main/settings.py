3dfb57efb5ed4ae8212d07f317a9c49fa0965c5c
vikas <vikask99588@gmail.com>
2022-08-20 12:40:13 +0530

    update

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!y1u#8)z%bj(r#2fpkp2+#c32mc8bif8r%yji4snl4^46nm%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hospital-vikas.herokuapp.com']
 
 
 # Application definition
